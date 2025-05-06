0b34bdbf958679dd5396f64490904cb357363f54
L <sintangoppo975@gmail.com>
2025-05-06 09:12:21 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-test
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+# Optional: agar model dikenali saat flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
+if __name__ == "__main__":
+    app.run(debug=True)
