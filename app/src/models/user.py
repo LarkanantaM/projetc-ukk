376290f4c76ea4bfaacee4c214c9511910540328
L <sintangoppo975@gmail.com>
2025-05-06 09:12:03 +0700

    Create user.py

---
+++ app/src/models/user.py
@@ -0,0 +1,13 @@
+# src/models/user.py
+
+class User:
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def check_password(self, password):
+        return self.password == password
+
+    def __repr__(self):
+        return f"User(username={self.username}, role={self.role})"
